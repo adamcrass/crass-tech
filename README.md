114936321a5ebad015fd91efe37e8242dcebe371
Adam Crass <aj.crass@gmail.com>
2025-05-18 09:51:52 -0700

    Initial Commit

---
+++ README.md
@@ -0,0 +1,1 @@
+Credits: [unicolors.app](https://uicolors.app/generate/406a86), [ChatGPT](https://chatgpt.com/#)
---
+++ src/App.js
@@ -0,0 +1,18 @@
+import "./App.css";
+import "./reset.css";
+
+function App() {
+  return (
+    <div className="App">
+      <nav className="App-navbar">
+        <a href="App.js">Thing one</a>
+        <a href="App.js">Thing two</a>
+        <a href="App.js">Thing three</a>
+        <a href="App.js">Thing four</a>
+      </nav>
+      <header className="App-header">hello</header>
+    </div>
+  );
+}
+
+export default App;
