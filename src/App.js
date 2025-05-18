cb4a04650ce27b2d76f96edf59e78e9a7df3f7e0
Adam Crass <aj.crass@gmail.com>
2025-05-18 10:57:56 -0700

    decided colors

--- src/App.js
+++ src/App.js
@@ -3,15 +3,18 @@ import "./reset.css";
 
 function App() {
   return (
-    <div className="App">
-      <nav className="App-navbar">
-        <a href="App.js">Thing one</a>
-        <a href="App.js">Thing two</a>
-        <a href="App.js">Thing three</a>
-        <a href="App.js">Thing four</a>
-      </nav>
-      <header className="App-header">hello</header>
-    </div>
+    <body>
+      <div className="App">
+        <nav className="App-navbar">
+          <a href="App.js">Thing one</a>
+          <a href="App.js">Thing two</a>
+          <a href="App.js">Thing three</a>
+          <a href="App.js">Thing four</a>
+        </nav>
+        <header className="App-header"></header>
+        <main className="App-main">This is a body</main>
+      </div>
+    </body>
   );
 }
 
