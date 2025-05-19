d6cb5ebe5317db616c4cf4bee6f433f8e42886ab
Adam Crass <aj.crass@gmail.com>
2025-05-19 10:31:25 -0700

    added big and small logo

--- README.md
+++ README.md
@@ -1,9 +1,1 @@
-<<<<<<< HEAD
 Credits: [unicolors.app](https://uicolors.app/generate/406a86), [ChatGPT](https://chatgpt.com/#)
-=======
-<<<<<<< HEAD
-# crass-tech
-=======
-Credits: [unicolors.app](https://uicolors.app/generate/406a86), [ChatGPT](https://chatgpt.com/#)
->>>>>>> 1149363 (Initial Commit)
->>>>>>> 49efded (Initial Commit)
--- src/App.js
+++ src/App.js
@@ -6,13 +6,22 @@ function App() {
     <body>
       <div className="App">
         <nav className="App-navbar">
-          <a href="App.js">Thing one</a>
-          <a href="App.js">Thing two</a>
-          <a href="App.js">Thing three</a>
-          <a href="App.js">Thing four</a>
+          <div className="navbar-left">
+            <img src="/CrassTechLogoNoWords.png" alt="Logo" id="small_logo" />
+          </div>
+          <div className="navbar-right">
+            <a href="App.js">Thing one</a>
+            <a href="App.js">Thing two</a>
+            <a href="App.js">Thing three</a>
+            <a href="App.js">Thing four</a>
+          </div>
         </nav>
-        <header className="App-header"></header>
-        <main className="App-main">This is a body</main>
+        <header className="App-header">
+          <img src="/CrassTechLogo3.png" alt="Logo" id="big_logo" />
+        </header>
+        <main className="App-main">
+          <div></div>
+        </main>
       </div>
     </body>
   );
